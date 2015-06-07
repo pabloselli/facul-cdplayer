1006fd17b38635915a506bdc2e367f769846e876
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2015-06-07 19:49:49 -0300

    Menu

--- MusicalRelax.cpp
+++ MusicalRelax.cpp
@@ -18,17 +18,57 @@ typedef struct list {
 }CD;
 
 void    entrada_dados  ( CD* aux ); // leitura dos dados de entrada
+void    cria_lista     ( CD** cd ); // inicia a lista
 
 int main(void){
 	setlocale(LC_ALL, "Portuguese");
 	
-	printf("------ Musical Relax ------");
-	printf("1 - Incluir CD.");
-	printf("2 - Excluír CD.");
-	printf("3 - Escolha de um CD para ser tocado e escolha da música.");
-	printf("4 - Mostra todos os CDs e as respectivas músicas.");
-	printf("5 - Mostrar e tocar TOP 10.");
-	printf("0 - Sair.");
+	int op;
+	CD* cd;
+	CD* topTen;
+	cria_lista(&cd);
+	cria_lista(&topTen);
+	
+	while(1){
+		system("cls");
+		printf("------ Musical Relax ------");
+		printf("1 - Incluir CD.");
+		printf("2 - Excluír CD.");
+		printf("3 - Escolha de um CD para ser tocado e escolha da música.");
+		printf("4 - Mostra todos os CDs e as respectivas músicas.");
+		printf("5 - Mostrar e tocar TOP 10.");
+		printf("0 - Sair.");
+		printf("Digite a opção desejada: ");
+		scanf("%d", &op);
+		
+		swtich(op){
+			case 1:
+				//Incluir
+				break;
+			case 2:
+				//Excluir
+				break;
+			case 3:
+				//Escutar
+				break;
+			case 4:
+				//Mostrar cds
+				break;
+			case 5:
+				//Mostar top 10
+				break;
+			case 0:
+				exit(1);
+				break;
+			default:
+				printf("Opção invárlida");
+				break;
+		}
+	}
+}
+
+void cria_lista( LISTA** l ){
+    *l = NULL;
 }
 
 void entrada_dados( CD* aux )
